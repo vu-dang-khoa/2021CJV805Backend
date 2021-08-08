32e63908d403448c4a5338cfb055515ef6e7b376
vu-dang-khoa <vu-dang-khoa@myseneca.ca>
2021-08-07 23:12:48 -0400

    cors

--- src/main/java/com/example/restapimongodb/controllers/AuthController.java
+++ src/main/java/com/example/restapimongodb/controllers/AuthController.java
@@ -10,12 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
+@CrossOrigin
 @RestController
 public class AuthController
 
--- src/main/java/com/example/restapimongodb/controllers/TvshowController.java
+++ src/main/java/com/example/restapimongodb/controllers/TvshowController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class TvshowController {
 
